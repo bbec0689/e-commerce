cb61dcedc82497b1e3ee54b36df4355883fe522b
Dave <bbec0689@gmail.com>
2021-03-28 18:17:21 -0600

    updated models and tag routes api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-
   Tag.findAll({
     include: [
       {
@@ -21,14 +19,13 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-
   Tag.findAll({
     where: {
       id: req.params.id,
     },
     include: [
       {
-        model: ProductTag,
+        model: Product,
       },
     ],
   })
